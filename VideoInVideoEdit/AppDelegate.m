cc15f3ac56670ed0dab1124cec1020ca9a7482fc
apple <153444073@qq.com>
2020-11-04 10:24:02 +0800

    Revert "1111"
    This reverts commit f0fb389bdd620ad1083c0e676f59ad8ee16511c6.

--- VideoInVideoEdit/AppDelegate.m
+++ VideoInVideoEdit/AppDelegate.m
@@ -13,7 +13,7 @@
 @end
 
 @implementation AppDelegate
-//addddddd
+
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
