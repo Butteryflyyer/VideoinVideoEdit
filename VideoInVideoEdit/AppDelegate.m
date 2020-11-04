af611e831bda5e784a0d28e1c6fe843a5ae463b3
apple <153444073@qq.com>
2020-11-04 16:08:47 +0800

    love2

--- VideoInVideoEdit/AppDelegate.m
+++ VideoInVideoEdit/AppDelegate.m
@@ -17,9 +17,7 @@
 
 
 //1111111
-//2222
-//ffffff
-
+//wwwwwwwwww
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     return YES;
